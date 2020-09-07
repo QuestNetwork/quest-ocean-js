3c4a477276346881668348e2d53c4bcefcecf8a2
StationedInTheField <ask@stationedinthefield.com>
2020-09-07 15:05:53 -0700

    init

---
+++ ocean.js
@@ -0,0 +1,50 @@
+// const qCaptcha = require('@questnetwork/quest-image-captcha-js');
+//
+// const axios = require('axios');
+// const CryptoJS = require('crypto-js')
+// const { v4: uuidv4 } = require('uuid');
+// import { Subject } from "rxjs";
+//
+// //provisional: https://github.com/PeculiarVentures/webcrypto/issues/19
+// let WebCrypto;
+// if(typeof(window.crypto) != 'undefined'){
+//   WebCrypto = window.crypto;
+// }
+// else{
+//   let { Crypto } = require("@peculiar/webcrypto");
+//   WebCrypto = new Crypto();
+// }
+//
+// function isInArray(value, array) {
+//   return array.indexOf(value) > -1;
+// }
+//
+// // import { GlobalQCaptcha as qCaptcha }  from '@questnetwork/quest-captcha-js';
+//
+
+export class Ocean {
+    constructor() {
+      //in the future only handle one channel per instanciated class
+      // this.ipfsCID = "";
+      // this.subs = {};
+      // this.channelParticipantList = {};
+      // this.channelKeyChain = {};
+      // this.channelNameList = [];
+      // this.splitter = "-----";
+      // this.channelHistory = {};
+      // let uVar;
+      // this.ipfsId = uVar;
+      // this.pubSubPeersSub = new Subject();
+      // this.DEVMODE = false;
+      // this.captchaCode = {};
+      // this.captchaRetries = {};
+      // this.commitNowSub = new Subject();
+      // this.inviteCodes = {};
+
+    }
+
+    isInArray(value, array) {
+     return array.indexOf(value) > -1;
+   }
+
+  }
